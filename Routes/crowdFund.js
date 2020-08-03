33ba0f78505be7c5fceb83f3b3d1d035b088d8ab
Vivek Regmi <49563455+vivek-regmi-au5@users.noreply.github.com>
2020-08-03 09:55:12 +0530

    Create crowdFund.js

---
+++ Routes/crowdFund.js
@@ -0,0 +1,89 @@
+const express = require("express");
+const router = express.Router();
+const passport = require("passport");
+const passportConf = require("./../passport");
+const CrowdFund = require("./../Models/crowdFund");
+
+// @route  Get api/crowdFund by curent user id
+// @desc   Get all crowd funded items for current user
+// @access Private
+router.get(
+  "/:profileId",
+
+  async (req, res, next) => {
+    try {
+      const crowdFundItems = await CrowdFund.find({
+        profileId: req.params.profileId,
+      })
+        .populate("profileId")
+        .populate("productId");
+      res.status(200).json(crowdFundItems);
+    } catch (error) {
+      res.send(error);
+    }
+  }
+);
+
+// @route  Post/ api/profile/address
+// @desc   Create address
+// @access Private
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res, next) => {
+    try {
+      const { profileId, productId } = req.body;
+      const crowdFundfields = {};
+      crowdFundfields.profileId = profileId;
+      crowdFundfields.productId = productId;
+      // Save an address
+      const crowdFundItem = new CrowdFund(crowdFundfields);
+      const newitem = await crowdFundItem.save();
+      res.status(201).json({ crowdFund: newitem });
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  }
+);
+
+// @route  Put api/crowdfund/id
+// @desc   Update crowdfunded item by adding a transcation id after someone donates money
+// @access Private
+router.put(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res, next) => {
+    const { transaction } = req.body;
+
+    //
+
+    try {
+      const crowdFundItem = await CrowdFund.findOneAndUpdate(
+        { _id: req.params.id },
+        { $push: { donationPool: transaction } },
+        { new: true }
+      );
+
+      res.status(201).json(crowdFundItem);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+// @route  Delete api/crowdFund/id
+// @desc   Delete address
+// @access Private
+router.delete("/:profileId/:productId", async (req, res, next) => {
+  try {
+    await CrowdFund.findOneAndDelete({
+      "profileId._id": req.params.profileId,
+      "productId._id": req.params.productId,
+    });
+    res.status(200).send("Item Successfully deleted");
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+module.exports = router;
